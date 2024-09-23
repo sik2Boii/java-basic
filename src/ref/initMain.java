dac32fd7780a6ae49b40b9fb67662561a9dddf63
Junsik <junsik213@naver.com>
2024-09-23 22:04:47 +0900

    변수와 초기화

---
+++ src/ref/initMain.java
@@ -0,0 +1,11 @@
+package ref;
+
+public class initMain {
+
+    public static void main(String[] args) {
+        InitData initData = new InitData();
+        System.out.println("value1: " + initData.value1);
+        System.out.println("value2: " + initData.value2);
+    }
+
+}
