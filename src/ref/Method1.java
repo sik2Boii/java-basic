8503ad8250bb4d7e681b09a3f74319b2a6b7c733
Junsik <junsik213@naver.com>
2024-09-23 21:59:11 +0900

    참조형과 메서드 호출의 활용

---
+++ src/ref/Method1.java
@@ -0,0 +1,33 @@
+package ref;
+
+public class Method1 {
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "학생1";
+        student1.age = 15;
+        student1.grade = 90;
+
+        Student student2 = new Student();
+        student2.name = "학생2";
+        student2.age = 16;
+        student2.grade = 80;
+
+        Student student3 = new Student();
+        initStudent(student3, "학생3", 17, 100);
+
+        printStudent(student1);
+        printStudent(student2);
+        printStudent(student3);
+    }
+
+    static void initStudent(Student student, String name, int age, int grade) {
+        student.name = name;
+        student.age = age;
+        student.grade = grade;
+    }
+
+    static void printStudent(Student student) {
+        System.out.println("이름: " + student.name + " 나이: " + student.age + " 성적: " + student.grade);
+    }
+}
