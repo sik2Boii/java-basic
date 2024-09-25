bc0441fb3abc25b5ca09a2438bd1b5637d84afca
Junsik <junsik213@naver.com>
2024-09-26 01:43:30 +0900

    다형성 적용 전 중복 코드의 문제점

---
+++ src/poly/ex1/AnimalSoundMain.java
@@ -0,0 +1,30 @@
+package poly.ex1;
+
+public class AnimalSoundMain {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Caw caw = new Caw();
+
+        // 배열과 for 문을 통한 중복 제거 시도
+        // 문제의 핵심은 타입이 다르다는 점
+        // 그래서 한 배열에 담기지 않음
+
+        System.out.println("동물 소리 테스트 시작");
+        dog.sound();
+        System.out.println("동물 소리 테스트 종료");
+
+        System.out.println("동물 소리 테스트 시작");
+        cat.sound();
+        System.out.println("동물 소리 테스트 종료");
+
+       sound(caw);
+    }
+
+    private static void sound(Caw caw) {
+        System.out.println("동물 소리 테스트 시작");
+        caw.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
